5d1f418a31fe00aa86c546299839980ed248e5fb
afeiship <1290657123@qq.com>
2021-05-26 11:30:21 +0800

    feat: add sticky

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -5,6 +5,7 @@ describe('api.basic', () => {
     expect(positionValue({ rel: true })).toBe('relative');
     expect(positionValue({ abs: true })).toBe('absolute');
     expect(positionValue({ fixed: true })).toBe('fixed');
+    expect(positionValue({ sticky: true })).toBe('sticky');
     expect(positionValue({ fixed: false })).toBe(null);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 type PositionValue = string | null;
 
 const TRUTHY_WARN = 'rel/abs/fixed should only have one truthy value.';
+
 export default (inProps): PositionValue => {
-  const { rel, abs, fixed } = inProps;
-  const props = [rel, abs, fixed];
+  const { rel, abs, fixed, sticky } = inProps;
+  const props = [rel, abs, fixed, sticky];
   const valied = props.filter(Boolean);
 
   valied.length > 1 && console.warn(TRUTHY_WARN);
@@ -11,5 +12,6 @@ export default (inProps): PositionValue => {
   if (rel) return 'relative';
   if (abs) return 'absolute';
   if (fixed) return 'fixed';
+  if (sticky) return 'sticky';
   return null;
 };
