27568423db62d108a9739027d6db446da769ecd4
afeiship <1290657123@qq.com>
2021-05-27 09:46:04 +0800

    refactor: valid wrong word

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,9 @@ const TRUTHY_WARN = 'rel/abs/fixed should only have one truthy value.';
 export default (inProps): PositionValue => {
   const { rel, abs, fixed, sticky } = inProps;
   const props = [rel, abs, fixed, sticky];
-  const valied = props.filter(Boolean);
+  const valid = props.filter(Boolean);
 
-  valied.length > 1 && console.warn(TRUTHY_WARN);
+  valid.length > 1 && console.warn(TRUTHY_WARN);
 
   if (rel) return 'relative';
   if (abs) return 'absolute';
