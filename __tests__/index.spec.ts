08dce5880c59d4fd58ec28d8dc3e475195469dca
afeiship <1290657123@qq.com>
2021-05-28 13:40:33 +0800

    fix: rel is conflict with html rel prop

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -3,6 +3,7 @@ import positionValue from '../src';
 describe('api.basic', () => {
   test('normail single value case', () => {
     expect(positionValue({ rel: true })).toBe('relative');
+    expect(positionValue({ relative: true })).toBe('relative');
     expect(positionValue({ abs: true })).toBe('absolute');
     expect(positionValue({ fixed: true })).toBe('fixed');
     expect(positionValue({ sticky: true })).toBe('sticky');
--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,13 @@ type PositionValue = string | null;
 const TRUTHY_WARN = 'rel/abs/fixed should only have one truthy value.';
 
 export default (inProps): PositionValue => {
-  const { rel, abs, fixed, sticky } = inProps;
-  const props = [rel, abs, fixed, sticky];
+  const { rel, relative, abs, fixed, sticky } = inProps;
+  const props = [rel, relative, abs, fixed, sticky];
   const valid = props.filter(Boolean);
 
   valid.length > 1 && console.warn(TRUTHY_WARN);
 
-  if (rel) return 'relative';
+  if (rel || relative) return 'relative';
   if (abs) return 'absolute';
   if (fixed) return 'fixed';
   if (sticky) return 'sticky';
